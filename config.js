f4e21769fba1b3f2ea643608a8dd57bff3eb829d
borjasalguero <fbsc@tid.es>
2015-05-21 12:32:36 +0100

    config added

---
+++ config.js
@@ -0,0 +1,21 @@
+(function(exports) {
+  var config = {
+    'performance': false,
+    'debug': false,
+    // Could be any value between
+    // ['list', 'detail', 'all']
+    'original-ui': []
+  }
+
+  exports.Config = {
+    get performance() {
+      return config.performance
+    },
+    get debug() {
+      return config.debug
+    },
+    get originalUI() {
+      return config['original-ui']
+    }
+  };
+}(this));
